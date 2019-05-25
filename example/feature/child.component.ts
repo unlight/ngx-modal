69cd6c2113eb46fc2be9d86c42823cbf8c2b2aa4
roman.vasilev <>
2019-05-26 02:03:40 +0400

    Removed onOpen for modal-confirms

--- example/feature/child.component.ts
+++ example/feature/child.component.ts
@@ -2,16 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     template: `
-        child 1 component
+        child component
         <a [routerLink]="['/', { outlets: { 'modal': 'lazyx'} }]">/ outlets: modal:'lazyx' </a> |
         <a [routerLink]="['/', { outlets: { 'modal': 'mxx'} }]">/ outlets: modal:'mxx'</a> |
         <-- this works only in app.component outlet with name=modal and  path: ':code'
-        (wich match modal with any name, but show ExampleModalComponent 1) but wee need
+        (wich match modal with any name, but show ExampleModalComponent 1) but we need
         <br/>
         <a [routerLink]="['.', { outlets: { 'lazy_modal': 'mxx'} }]">/ outlets: modal:'mxx' lazy modal</a> <br/>
         <a [routerLink]="['./children_lazy_modal_component']">primary children_lazy_modal_comp</a> <br/>
 
     `
 })
-export class ChildComponent {
-}
+export class ChildComponent {}
